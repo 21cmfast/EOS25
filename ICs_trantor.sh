35d6438a6aa99479ac6dddb0ee532627e22f617a
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2025-07-21 17:08:55 +0200

    maint: add trantor script

---
+++ ICs_trantor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#PBS -l select=1:ncpus=16:ngpus=0:mem=2000gb:mpiprocs=16
+#PBS -q q02hal
+#PBS -m bea
+#PBS -M daniela.breitman@sns.it
+#PBS -N EOS25-ICs
+conda activate 21cmFASTv4
+module load gcc/9.3.0
+cd /home/dbreitman/EOS25/EOS25
+21cmfast run ics --param-file EOS25.toml --seed 1234 --cache_dir "/projects/cosmo_eos/dbreitman/EOS25/EOS25_L4095_HIIDIM1365_DIM2048"
+wait
