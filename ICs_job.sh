b79a8939506799b2c9965a02893821eba859abb6
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2025-07-23 19:17:06 +0200

    maint: fix ICs

--- ICs_job.sh
+++ ICs_job.sh
@@ -14,4 +14,4 @@ module load openmpi/5.0.3-gcc13.2.1
 conda activate 21cmFASTv4
 cd /jet/home/breitman/EOSv4
 
-21cmfast run ics --param-file EOS25.toml --seed 1234 --cachedir "/ocean/projects/phy210034p/breitman/EOS25/EOS25_L2100_HIIDIM1400_DIM4200"
+21cmfast run ics --param-file EOS25.toml --seed 1234 --cachedir "/ocean/projects/phy210034p/breitman/EOS25/EOS25_L2100_HIIDIM1400_DIM4200" --min-evolved-redshift 5.0 --zprime-step-factor 1.02 --z-heat-max 35.0
--- run_evol.py
+++ run_evol.py
@@ -2,10 +2,8 @@ import py21cmfast as p21c
 from py21cmfast.io.caching import RunCache
 import numpy as np
 from template2input import create_params_from_template
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--z_idx", type = int)
-z_idx = parser.parse_args().z_idx
+from py21cmfast import generate_coeval
+
 
 cache = p21c.OutputCache('/home/dbreitman/EOS25/EOS25_L600_HIIDIM400_DIM1200_NO_PERTURN_ON_HIGH_RES')
 
@@ -13,11 +11,14 @@ inputs = p21c.InputParameters.from_template("EOS25.toml", random_seed=1234)
 runcache = RunCache.from_inputs(inputs, cache=cache)
 initial_conditions = runcache.get_ics()
 inputs = initial_conditions.inputs  # use the real inputs, with correct node_redshifts
-z = inputs.node_redshifts[z_idx]
 
-p21c.perturb_field(redshift=z, 
-                   initial_conditions=initial_conditions,
-                   write=True,
-                   cache=cache,
-)
-    
+for i in range(1):
+    next(generate_coeval(
+    inputs = inputs,
+    regenerate = False,
+    write = True,
+    cache = cache,
+    initial_conditions = initial_conditions,
+    progressbar = True,
+    )
+    )
