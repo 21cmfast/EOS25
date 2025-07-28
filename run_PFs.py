94b8b9fdc464a233d556f5d74b161450348a61ae
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2025-07-28 15:58:24 +0200

    scripts for PFs

--- run_PFs.py
+++ run_PFs.py
@@ -3,13 +3,13 @@ from py21cmfast.io.caching import RunCache
 import numpy as np
 from template2input import create_params_from_template
 import argparse
-parser = argparse.ArgumentParser()
+
 parser.add_argument("--z_idx", type = int)
 z_idx = parser.parse_args().z_idx
 
-cache = p21c.OutputCache('/home/dbreitman/EOS25/TEST_L600_HIIDIM400_DIM1200_NO_PERTURN_ON_HIGH_RES')
+cache = p21c.OutputCache('/ocean/projects/phy210034p/breitman/EOS25/EOS25_L2100_HIIDIM1400_DIM4200')
 
-inputs = p21c.InputParameters.from_template("/home/dbreitman/EOS25/TEST_L600_HIIDIM400_DIM1200_NO_PERTURN_ON_HIGH_RES/config-7dd812.toml", random_seed=1234)
+inputs = p21c.InputParameter.from_template("EOS25.toml", random_seed=1234)
 runcache = RunCache.from_inputs(inputs, cache=cache)
 initial_conditions = runcache.get_ics()
 inputs = initial_conditions.inputs  # use the real inputs, with correct node_redshifts
