32015537e8136b94c6d81130aab2bd12cde2795b
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2025-07-28 15:38:47 +0200

    scripts for ICs and PFs

--- ICs_job.sh
+++ ICs_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p EM
-#SBATCH -t 12:00:00
+#SBATCH -t 20:00:00
 #SBATCH --ntasks-per-node=72
 
 #echo commands to stdout
@@ -14,4 +14,5 @@ module load openmpi/5.0.3-gcc13.2.1
 conda activate 21cmFASTv4
 cd /jet/home/breitman/EOSv4
 
-21cmfast run ics --param-file EOS25.toml --seed 1234 --cachedir "/ocean/projects/phy210034p/breitman/EOS25/EOS25_L2100_HIIDIM1400_DIM4200" --min-evolved-redshift 5.0 --zprime-step-factor 1.02 --z-heat-max 35.0
+21cmfast run ics --param-file EOS25.toml --seed 1234 --cachedir "/ocean/projects/phy210034p/breitman/EOS25/EOS25_L2100_HIIDIM1400_DIM4200" --min-evolved-redshift 5.0 --zprime-step-factor 1.02
+bash submit_PF_jobs.sh
