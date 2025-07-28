5b6f7e15d2a04d92985de649d16c49cda0b7e71a
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2025-07-28 16:16:16 +0200

    PHFs scripts

--- run_PHFs.py
+++ run_PHFs.py
@@ -2,12 +2,12 @@ import py21cmfast as p21c
 from py21cmfast.io.caching import RunCache, CacheConfig
 import numpy as np
 
-p21c.config['HALO_CATALOG_MEM_FACTOR'] = 2.
+#p21c.config['HALO_CATALOG_MEM_FACTOR'] = 2.
 
-cache = p21c.OutputCache('/home/dbreitman/EOS25/TEST_L600_HIIDIM400_DIM1200_NO_PERTURN_ON_HIGH_RES')
+cache = p21c.OutputCache('/ocean/projects/phy210034p/breitman/EOS25/EOS25_L2100_HIIDIM1400_DIM4200')
 
 
-inputs = p21c.InputParameters.from_template("/home/dbreitman/EOS25/TEST_L600_HIIDIM400_DIM1200_NO_PERTURN_ON_HIGH_RES/config-7dd812.toml", random_seed=1234)
+inputs = p21c.InputParameters.from_template("EOS25.toml", random_seed=1234)
 runcache = RunCache.from_inputs(inputs, cache=cache)
 initial_conditions = runcache.get_ics()
 inputs = initial_conditions.inputs  # use the real inputs, with correct node_redshifts
