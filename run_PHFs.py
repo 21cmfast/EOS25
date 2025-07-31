3f6e55b2882c713a7ebb4ec62ea5c96b2141c350
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2025-07-31 10:41:51 +0200

    fix: add node redshifts in PHFs script

--- run_PHFs.py
+++ run_PHFs.py
@@ -7,16 +7,17 @@ p21c.config['HALO_CATALOG_MEM_FACTOR'] = 1.4
 cache = p21c.OutputCache('/ocean/projects/phy210034p/breitman/EOS25/EOS25_L2100_HIIDIM1400_DIM4200')
 
 
-inputs = p21c.InputParameters.from_template("EOS25.toml", random_seed=1234)
+inputs = p21c.InputParameters.from_template("EOS25.toml", random_seed=1234,  node_redshifts=p21c.get_logspaced_redshifts(min_redshift=5.0,z_step_factor=1.02, max_redshift=35.0))
 runcache = RunCache.from_inputs(inputs, cache=cache)
 initial_conditions = runcache.get_ics()
 inputs = initial_conditions.inputs  # use the real inputs, with correct node_redshifts
-
+print("Got ICs, starting evolve perturb halos")
 p21c.drivers.coeval.evolve_perturb_halos(inputs=inputs,
                     initial_conditions=initial_conditions,
                     regenerate=False,
                     all_redshifts=inputs.node_redshifts,
                     write=CacheConfig(),
                     cache=cache,
+                    progressbar=True,
 )
     
